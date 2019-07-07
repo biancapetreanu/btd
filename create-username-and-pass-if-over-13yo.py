e695d5878faa8d5de74c4c0fff15f69d6dc7759b
biancapetreanu <52544118+biancapetreanu@users.noreply.github.com>
2019-07-07 13:28:37 +0300

    Added condition

---
+++ create-username-and-pass-if-over-13yo.py
@@ -0,0 +1,3 @@
+from django.contrib import Auth
+if form_age>13:
+ user1 = user.objects.create_user('example', 'example@example.com', 'passexample')
--- create-username-and-pass.py
+++
@@ -1,2 +0,0 @@
-from django.contrib.auth.models import User
-user1 = user.objects.create_user('example', 'example@example.com', 'passexample')
